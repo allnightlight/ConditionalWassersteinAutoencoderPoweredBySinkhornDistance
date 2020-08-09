a03c441a28855f650ea35f700917e7398c4c8e01
Shouta Ukai <shouta.ukai@gmail.com>
2020-08-09 14:59:23 +0900

    Modified hyperparameter to see the resolution of Wasserstein dist.

--- casestudies/conc_build_parameter.py
+++ casestudies/conc_build_parameter.py
@@ -8,8 +8,8 @@ from wae_build_parameter import WaeBuildParameter
 class ConcBuildParameter(WaeBuildParameter):
     
     
-    def __init__(self, nIntervalSave=2 ** 4, nEpoch=2 ** 7, label="None", nBatch=2 ** 5, nH=2 ** 3, nXi=2, nLayer=1, reg_param=0.1, cluster_interval=3.0, target_casestudy = "cs01a"):
-        WaeBuildParameter.__init__(self, nIntervalSave=nIntervalSave, nEpoch=nEpoch, label=label, nBatch=nBatch, nH=nH, nXi=nXi, nLayer=nLayer, reg_param=reg_param, cluster_interval=cluster_interval)
+    def __init__(self, nIntervalSave=2 ** 4, nEpoch=2 ** 7, label="None", nBatch=2 ** 5, nH=2 ** 3, nXi=2, nLayer=1, reg_param=0.1, cluster_interval=3.0, tol_sinkhorn=0.1, eps_given_sinkhorn=0.1, target_casestudy = "cs01a"):
+        WaeBuildParameter.__init__(self, nIntervalSave=nIntervalSave, nEpoch=nEpoch, label=label, nBatch=nBatch, nH=nH, nXi=nXi, nLayer=nLayer, reg_param=reg_param, cluster_interval=cluster_interval, tol_sinkhorn=tol_sinkhorn, eps_given_sinkhorn=eps_given_sinkhorn)
     
         
         self.target_casestudy = target_casestudy
