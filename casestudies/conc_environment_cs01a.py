6b5e5492d97af327f57ff762bafdf599f878b87b
Shouta Ukai <shouta.ukai@gmail.com>
2020-08-10 19:48:30 +0900

    Modified the environment for cs01a to fix the random seed

--- casestudies/conc_environment_cs01a.py
+++ casestudies/conc_environment_cs01a.py
@@ -12,7 +12,7 @@ class ConcEnvironmentCs01a(WaeEnvironment):
     '''
 
 
-    def __init__(self, nBatch, d_out = 2, d_in = 1):
+    def __init__(self, nBatch, d_out = 2, d_in = 1, seed = 0, nSample = 2**10):
         WaeEnvironment.__init__(self, nBatch)
         
         self.nX = 2
@@ -20,16 +20,18 @@ class ConcEnvironmentCs01a(WaeEnvironment):
 
         self.d_out = d_out
         self.d_in = d_in
+        self.randomState = np.random.RandomState(seed=seed)
+        self.nSample = nSample
         
     def loadData(self):
-        nSample = 2**10
+        nSample = self.nSample
         d_out = self.d_out
         d_in = self.d_in
 
         f = lambda y: np.exp(y * np.log(d_out/d_in)) * d_in/2
-        y = np.random.rand(nSample)
+        y = self.randomState.rand(nSample)
         r = f(y)
-        theta = np.random.rand(nSample) * np.pi * 2
+        theta = self.randomState.rand(nSample) * np.pi * 2
         X = np.stack((r * np.cos(theta), r * np.sin(theta)), axis=1) # (*,2)
 
         Z = np.zeros((nSample, self.nZ))
